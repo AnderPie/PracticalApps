e49f3e0cf7c5ed7882568c93650bb8da49a6b20f
Ander Pierce <Anderpierce@protonmail.com>
2025-01-28 19:21:08 -0800

    Working on exercise 13.8

--- Northwind.Mvc/obj/Debug/net8.0/Northwind.Mvc.AssemblyInfo.cs
+++ Northwind.Mvc/obj/Debug/net8.0/Northwind.Mvc.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d873cba8e53335f00b161076149bfbefdb1538fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35b7cf59b75021fb7a4d1dc511de4091f1013a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
