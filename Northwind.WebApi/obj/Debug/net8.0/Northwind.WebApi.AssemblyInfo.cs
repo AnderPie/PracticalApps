d625ae7930aab0dc4ff044074c8480804ec87c10
Ander Pierce <Anderpierce@protonmail.com>
2025-02-11 14:23:25 -0800

    Bringing main up-to-date

--- Northwind.WebApi/obj/Debug/net8.0/Northwind.WebApi.AssemblyInfo.cs
+++ Northwind.WebApi/obj/Debug/net8.0/Northwind.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26e6ed6bf5ec4b19e08f8f4281136547d623421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93dbe6f26e26fd552ba9246721605495da136b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
